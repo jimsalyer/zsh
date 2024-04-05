1add35349b3abaf574f0233ee979f6741c9a70ba
Jim Salyer <jim.salyer@gmail.com>
2024-04-05 12:12:09 -0500

    Remove WWT specific exports. JS

--- scripts/exports.zsh
+++
@@ -1,6 +0,0 @@
-export AWS_CA_BUNDLE=/Library/Application\ Support/Netskope/STAgent/data/netskope-cert-bundle.pem
-export CURL_CA_BUNDLE=/Library/Application\ Support/Netskope/STAgent/data/netskope-cert-bundle.pem
-export NODE_EXTRA_CA_CERTS=/Library/Application\ Support/Netskope/STAgent/data/nscacert_combined.pem
-export SSL_CERT_FILE=/Library/Application\ Support/Netskope/STAgent/data/netskope-cert-bundle.pem
-export GIT_SSL_CAPATH=/Library/Application\ Support/Netskope/STAgent/data/netskope-cert-bundle.pem
-export REQUESTS_CA_BUNDLE=/Library/Application\ Support/Netskope/STAgent/data/netskope-cert-bundle.pem
